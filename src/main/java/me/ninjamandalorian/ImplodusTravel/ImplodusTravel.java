6b5ff956d2a8212d6174f09605738232d645873a
Isaac <ninjamandalov@gmail.com>
2023-09-03 00:47:37 +0100

    Added init

--- src/main/java/me/ninjamandalorian/ImplodusTravel/ImplodusTravel.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/ImplodusTravel.java
@@ -1,21 +1,49 @@
 package me.ninjamandalorian.ImplodusTravel;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.economy.Economy;
+
 public class ImplodusTravel extends JavaPlugin {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+    private static ImplodusTravel instance;
+    public static Economy econ;
+
 
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info(getName() + " enabled.");
+        instance = this;
+
+        setupEconomy();
+        PluginManager pluginManager = getServer().getPluginManager();
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getLogger().info(getName() + " disabled.");
+
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static ImplodusTravel getInstance() {
+        return instance;
+    }
+
+    public static Economy getEcon() {
+        return econ;
     }
 
 }
