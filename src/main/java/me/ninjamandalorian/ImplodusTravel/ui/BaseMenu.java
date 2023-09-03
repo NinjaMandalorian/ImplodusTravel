5b6470a4c8fb57e3e950d64c5a6c7a16e42ed991
Isaac <ninjamandalov@gmail.com>
2023-09-03 20:20:51 +0100

    Formatting update

--- src/main/java/me/ninjamandalorian/ImplodusTravel/ui/BaseMenu.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/ui/BaseMenu.java
@@ -211,7 +211,7 @@ public class BaseMenu implements InventoryHolder {
         
         PagedBuilder() {
             List<Integer> outlineSlots = Arrays.asList(0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,44,
-                                                       45,46,47,48,49,50,51,52,53);
+            45,46,47,48,49,50,51,52,53);
             for (Integer slot : outlineSlots) {
                 menuButtons.put(slot, BaseButton.background());
             }
