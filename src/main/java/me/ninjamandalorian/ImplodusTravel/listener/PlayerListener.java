f37ceac0025df0164f0d77a990076bb475bbb590
Isaac <ninjamandalov@gmail.com>
2023-09-03 18:55:11 +0100

    Added open menu logic

--- src/main/java/me/ninjamandalorian/ImplodusTravel/listener/PlayerListener.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/listener/PlayerListener.java
@@ -2,16 +2,13 @@ package me.ninjamandalorian.ImplodusTravel.listener;
 
 import java.util.HashMap;
 
-import org.bukkit.NamespacedKey;
 import org.bukkit.block.Banner;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.persistence.PersistentDataType;
-
-import me.ninjamandalorian.ImplodusTravel.ImplodusTravel;
 
 public class PlayerListener implements Listener {
     
@@ -27,16 +24,13 @@ public class PlayerListener implements Listener {
         cooldownMap.put(player, System.currentTimeMillis());
 
         if (block.getState() instanceof Banner banner) {
-            NamespacedKey key = new NamespacedKey(ImplodusTravel.getInstance(), "banner-count");
-            player.sendMessage(""+banner.getPersistentDataContainer().has(key, PersistentDataType.INTEGER));
-            if(!banner.getPersistentDataContainer().has(key, PersistentDataType.INTEGER)) {
-                banner.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, 0);
-                banner.update();
-            } else {
-                Integer count = banner.getPersistentDataContainer().get(key, PersistentDataType.INTEGER);
-                banner.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, count + 1);
-                banner.update();
-                player.sendMessage(count + " - " + banner.getPersistentDataContainer().get(key, PersistentDataType.INTEGER));
+            
+            // TODO If NOT station return
+            Action action = e.getAction();
+            if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
+                e.setCancelled(true);
+                // TODO Open menu
+                player.sendMessage("OPEN MENU");
             }
         }
         
