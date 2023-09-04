8edcd5703f7e4df9ba8f70bfc09a742f384696eb
Isaac <ninjamandalov@gmail.com>
2023-09-04 01:38:30 +0100

    Added StationMenu class w/ outlines

---
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/ui/StationMenu.java
@@ -0,0 +1,21 @@
+package me.ninjamandalorian.ImplodusTravel.ui;
+
+import org.bukkit.entity.Player;
+
+import me.ninjamandalorian.ImplodusTravel.object.Station;
+
+public class StationMenu {
+    
+    public static BaseMenu stationMenu(Player player, Station station) {
+        return null;
+    }
+
+    public static BaseMenu stationListMenu(Player player, Station station) {
+        return null;
+    }
+
+    public static BaseMenu stationConfigMenu(Player player, Station station) {
+        return null;
+    }
+
+}
