2f50349f6b3155e576f55963e1677e39b28db410
Isaac <ninjamandalov@gmail.com>
2023-09-10 01:48:56 +0100

    Added BlockListener class

---
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/listener/BlockListener.java
@@ -0,0 +1,66 @@
+package me.ninjamandalorian.ImplodusTravel.listener;
+
+import org.bukkit.Nameable;
+import org.bukkit.block.Banner;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+
+import me.ninjamandalorian.ImplodusTravel.controller.PersistentDataController;
+import net.md_5.bungee.api.ChatColor;
+
+public class BlockListener implements Listener {
+    
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        Player player = e.getPlayer();
+        Block block = e.getBlock();
+        
+        if (player != null && block != null) return;
+        // if (block.getType().toString().contains("SIGN")) {
+        //     Port port = Port.getPort(block.getLocation());
+        //     if (port == null) return;
+            
+        //     if (player.hasPermission("implodusports.admin.destroy")) {
+        //         Port.portDestroy(player, port);
+        //         return;
+        //     } else {
+        //         e.setCancelled(true);
+        //         player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9[&6iPorts&9] &cYou do not have permission to destroy this."));
+        //         return;
+        //     }
+        // }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Player player = e.getPlayer();
+        ItemStack item = e.getItemInHand();
+        
+        
+
+        if (PersistentDataController.isStationItem(item) == false) return;
+        if (!player.hasPermission("implodustravel.station.place")) {
+            e.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "You cannot place this.");
+            return;
+        }
+
+        Block block = e.getBlock();
+        PersistentDataController.giveCustomTag(block);
+        player.sendMessage("" + block.getType().toString() + (block.getState() instanceof Nameable ));
+        BlockState state = block.getState();
+        Nameable nameable = (Nameable) state;
+        nameable.setCustomName("Banner");
+        state.update();
+        
+
+    }
+    
+}
