3c5efc138840c7b9f069981936c98ef6ac955c6f
Isaac <ninjamandalov@gmail.com>
2023-09-26 16:50:14 +0100

    Added closeMenu() functionality to RunnableTask

--- src/main/java/me/ninjamandalorian/ImplodusTravel/ui/task/RunnableTask.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/ui/task/RunnableTask.java
@@ -4,15 +4,22 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class RunnableTask implements BaseTask {
 
-    Runnable runnable;
+    private Runnable runnable;
+    private boolean closeMenu = false;
 
     public RunnableTask(Runnable runnable) {
         this.runnable = runnable;
     }    
 
+    public RunnableTask closeMenu() {
+        closeMenu = !closeMenu;
+        return this;
+    }
+
     @Override
     public void run(InventoryClickEvent event) {
         runnable.run();
+        if (closeMenu) event.getWhoClicked().closeInventory();
     }
     
 }
