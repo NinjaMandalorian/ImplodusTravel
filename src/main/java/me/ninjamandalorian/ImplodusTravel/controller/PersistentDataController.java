835cfa3245ff9e0050fba31cb836579bf8f527a4
Isaac <ninjamandalov@gmail.com>
2023-09-26 16:49:26 +0100

    Added token tags to PDC

--- src/main/java/me/ninjamandalorian/ImplodusTravel/controller/PersistentDataController.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/controller/PersistentDataController.java
@@ -1,5 +1,7 @@
 package me.ninjamandalorian.ImplodusTravel.controller;
 
+import java.util.UUID;
+
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Banner;
 import org.bukkit.block.Block;
@@ -11,11 +13,13 @@ import org.bukkit.persistence.PersistentDataHolder;
 import org.bukkit.persistence.PersistentDataType;
 
 import me.ninjamandalorian.ImplodusTravel.ImplodusTravel;
+import me.ninjamandalorian.ImplodusTravel.object.Station;
 
 public class PersistentDataController {
     
     private static NamespacedKey customKey = new NamespacedKey(ImplodusTravel.getInstance(), "customcheck");
-    
+    private static NamespacedKey tokenKey = new NamespacedKey(ImplodusTravel.getInstance(), "stationCode");
+
     public static boolean isStationItem(ItemStack item) {
         if (item.getItemMeta().getPersistentDataContainer().has(customKey, PersistentDataType.INTEGER)) return true;
         return false;
@@ -28,6 +32,20 @@ public class PersistentDataController {
         return false;
     }
 
+    public static UUID getTokenUUID(ItemStack item) {
+        if (item == null || item.getItemMeta() == null) return null;
+        PersistentDataContainer pdc = item.getItemMeta().getPersistentDataContainer();
+        if (!pdc.has(tokenKey, PersistentDataType.STRING)) {
+            return null;
+        }
+        try {
+            UUID stationUUID = UUID.fromString(pdc.get(tokenKey, PersistentDataType.STRING));
+            return stationUUID;
+        } catch (IllegalArgumentException ex) {
+            return null;
+        }
+    }
+
     public static void giveCustomTag(Block block) {
         BlockState state = block.getState();
         if (state instanceof PersistentDataHolder holder) {
@@ -37,9 +55,20 @@ public class PersistentDataController {
     }
 
     public static void giveItemTag(ItemStack item) {
+        giveItemTag(item, 2);
+    }
+
+    public static void giveItemTag(ItemStack item, Integer number) {
         ItemMeta meta = item.getItemMeta();
         PersistentDataContainer pdc = meta.getPersistentDataContainer();
-        pdc.set(customKey, PersistentDataType.INTEGER, 2);
+        pdc.set(customKey, PersistentDataType.INTEGER, number);
+        item.setItemMeta(meta);
+    }
+
+    public static void giveTokenTag(ItemStack item, Station station) {
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+        pdc.set(tokenKey, PersistentDataType.STRING, station.getIdString());
         item.setItemMeta(meta);
     }
 
