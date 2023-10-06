fb264fc33f48e49e57d61c475a35b2687df5aaf9
Isaac <ninjamandalov@gmail.com>
2023-10-06 13:37:21 +0100

    Data Documentation

--- src/main/java/me/ninjamandalorian/ImplodusTravel/data/StationDataManager.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/data/StationDataManager.java
@@ -16,26 +16,33 @@ import org.bukkit.World;
 import me.ninjamandalorian.ImplodusTravel.Logger;
 import me.ninjamandalorian.ImplodusTravel.object.Station;
 
+/** Station Data Manager
+ * @author NinjaMandalorian
+ */
 public class StationDataManager {
     
-
+    /** Init for station data
+     * @param dataFolderPath - Data folder path
+     * @return List of stations created
+     */
     static List<Station> initStationFiles(String dataFolderPath) {
         ArrayList<Station> returnList = new ArrayList<>();
         
-        File folder = new File(dataFolderPath + File.separator + "stations");
+        File folder = new File(dataFolderPath + File.separator + "stations"); // Folder for station data
 
+        // Create filter
         FilenameFilter filter = new FilenameFilter() {
             public boolean accept(File f, String name) {
                 // Ignores non .yml files
                 return name.endsWith(".yml");
             }
         };
-
+        // Gets files from folder
         File[] files = folder.listFiles(filter);
         for (int i = 0; i < files.length; i++) {
             try {
             HashMap<String,Object> map = DataManager.getData("stations" + File.separator + files[i].getName());
-            Station station = mapToStation(map);
+            Station station = mapToStation(map); // Makes station out of map
             returnList.add(station);
             } catch (Exception e) {
                 Logger.warn("Caught error when producing station " + files[i].getName());
@@ -46,6 +53,10 @@ public class StationDataManager {
         return returnList;
     }
 
+    /** Converts map to station
+     * @param hashMap - Map
+     * @return Station
+     */
     @SuppressWarnings("unchecked")
     private static Station mapToStation(HashMap<String, Object> hashMap) {
 
@@ -58,7 +69,7 @@ public class StationDataManager {
         Location destination = stringToLocation((String) hashMap.get("destination"));
 
         Station station = new Station(uuid, displayName, owner, location, destination);
-
+        // Creates destinations if in map
         if (hashMap.containsKey("destinationStations")) {
             List<String> destinationList = (List<String>) hashMap.get("destinationStations");
             for (String string : destinationList) {
@@ -68,18 +79,18 @@ public class StationDataManager {
                 } catch (IllegalArgumentException e) {}
             }
         }
-
+        // Creates ranks if in map
         if (hashMap.containsKey("rankMultipliers")) {
             HashMap<String, Double> multplierMap = (HashMap<String, Double>) hashMap.get("rankMultipliers");
             for (Entry<String, Double> entry : multplierMap.entrySet()) {
                 station.setRankMult(entry.getKey(), entry.getValue());
             }
         }
-
+        // Sets cost if in map
         if (hashMap.containsKey("defaultCost")) {
             station.setDefaultCost((Double) hashMap.get("defaultCost"));
         }
-
+        // Sets owner only if in map
         if (hashMap.containsKey("ownerOnlyMaps")) {
             station.setOwnerOnlyMaps((Boolean) hashMap.get("ownerOnlyMaps"));
         }
@@ -87,6 +98,10 @@ public class StationDataManager {
         return station;
     }
 
+    /** Creates a map from a station
+     * @param station - Station to convert
+     * @return HashMap of Station
+     */
     private static HashMap<String, Object> stationToMap(Station station) {
         HashMap<String,Object> map = new HashMap<String, Object>();
         map.put("uuid", station.getId().toString());
@@ -113,6 +128,10 @@ public class StationDataManager {
         return map;
     }
 
+    /** Converts string into location
+     * @param string - Location string
+     * @return Location object
+     */
     private static Location stringToLocation(String string) {
         // Format: "Overworld:123.0:456.0:789.0:90.0:0.0:"
         String[] stringList = string.split(":");
@@ -125,10 +144,18 @@ public class StationDataManager {
         return null;
     }
 
+    /** Converts location to string
+     * @param location - Location object
+     * @return Location as string
+     */
     private static String locationToString(Location location) {
         return location.getWorld().getUID().toString() + ":" + location.getX() + ":" + location.getY() + ":" + location.getZ() + ":" + location.getYaw() + ":" + location.getPitch();
     }
 
+    /** Saves station as file
+     * @param station - Station to save
+     * @throws Exception - Failure converting to map
+     */
     public static void saveStation(Station station) throws Exception {
         HashMap<String, Object> stationMap = stationToMap(station);
 
@@ -136,6 +163,7 @@ public class StationDataManager {
         DataManager.saveData(filePath, stationMap);
     }
 
+    /** Saves all stations */
     public static void saveAllStations() {
         for (Station station : Station.getStations()) {
             try {
@@ -146,6 +174,10 @@ public class StationDataManager {
         }
     }
 
+    /** Deletes station
+     * @param station - Station to delete
+     * @throws Exception - Failure to delete
+     */
     public static void deleteStation(Station station) throws Exception {
         DataManager.deleteFile("stations" + File.separator + station.getIdString() + ".yml");
     }
