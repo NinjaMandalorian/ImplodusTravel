1067eed70b311ebb1246867feb93124a6195be92
Isaac <ninjamandalov@gmail.com>
2023-10-18 02:17:00 +0100

    Listener documentation

--- src/main/java/me/ninjamandalorian/ImplodusTravel/listener/AsyncChatListener.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/listener/AsyncChatListener.java
@@ -12,21 +12,29 @@ import me.ninjamandalorian.ImplodusTravel.object.SettingRequest;
 import me.ninjamandalorian.ImplodusTravel.settings.Settings;
 import net.md_5.bungee.api.ChatColor;
 
+/** Chat skimmer for settings
+ * @author NinjaMandalorian
+ */
 public class AsyncChatListener implements Listener {
     
-    private static HashMap<Player, SettingRequest> settingRequests = new HashMap<>();
+    private static HashMap<Player, SettingRequest> settingRequests = new HashMap<>(); // Map for handling requests
 
+    /** Runs on a player sending a message in chat
+     * @param e - Event
+     */
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         if (settingRequests.containsKey(e.getPlayer())) {
+            // If player is in map, gets the request and removes it from the list
             Player player = e.getPlayer();
             SettingRequest request = settingRequests.get(player);
             settingRequests.remove(e.getPlayer());
             if (request.getRequestTime() + (1000L * Settings.getSettingTimeout()) < System.currentTimeMillis()) {
                 player.sendMessage(ChatColor.RED + "Setting request expired. Please try again.");
-                return;
+                return; // If past expiry date, return
             }
 
+            // Try to set setting, catches exception to show player.
             try {
                 request.getSettable().setSetting(request.getSetting(), e.getMessage());
             } catch (ChatSettingException ex) {
@@ -35,14 +43,17 @@ public class AsyncChatListener implements Listener {
             try {
                 for (Player p : e.getRecipients()) {
                     if (!p.equals(player)) {
+                        // Hides player's chat message for all other players
                         e.getRecipients().remove(p);
                     }
                 }   
             } catch (Exception ex) {}
-            // e.setCancelled(true);
         }
     }
 
+    /** Starts a request
+     * @param request - Request to be added to checks
+     */
     public static void startRequest(SettingRequest request) {
         settingRequests.put(request.getPlayer(), request);
     }
--- src/main/java/me/ninjamandalorian/ImplodusTravel/listener/BlockListener.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/listener/BlockListener.java
@@ -19,67 +19,77 @@ import me.ninjamandalorian.ImplodusTravel.object.Station;
 import me.ninjamandalorian.ImplodusTravel.settings.Settings;
 import net.md_5.bungee.api.ChatColor;
 
+/** Listener for block events
+ * @author NinjaMandalorian
+ */
 public class BlockListener implements Listener {
     
+    /** Block break listener
+     * @param e - Event
+     */
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
         Player player = e.getPlayer();
         Block block = e.getBlock();
         
         if (block.getState() instanceof Banner banner) {
-            
-            if (!PersistentDataController.isStationBlock(block)) return;
+            if (!PersistentDataController.isStationBlock(block)) return; // Ignores if not station
             Station station = Station.getStation(block.getLocation());
-            if (station == null || player.hasPermission("implodustravel.admin")) {
-                if (station != null) station.delete();
+            if (station == null || player.hasPermission("implodustravel.admin")) { // Checks if admin or null station
+                if (station != null) station.delete(); // Deletes station object
                 Logger.log("Player " +  player.getUniqueId().toString() + " destroyed station banner " + String.valueOf(station));
             } else {
-                e.setCancelled(true);
+                e.setCancelled(true); // Cancels and notifies player
                 player.sendMessage(ChatColor.RED + "You cannot break this station.");
             }
         }
     }
 
+    /** Block placed listener
+     * @param e - Event
+     */
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
         Player player = e.getPlayer();
         ItemStack item = e.getItemInHand();
-        
-        
 
-        if (PersistentDataController.isStationItem(item) == false) return;
+        if (PersistentDataController.isStationItem(item) == false) return; // Ignores if not station item
         if (!player.hasPermission("implodustravel.station.place")) {
-            e.setCancelled(true);
+            e.setCancelled(true); // Cancels if player lacks permission & 
             player.sendMessage(ChatColor.RED + "You cannot place this.");
             return;
         }
 
+        // Max station check
         if (Station.getPlayerStations(player).size() >= Settings.getMaxStationsPerPerson()) {
             if (player.hasPermission("implodustravel.admin")) {
+                // Admins can override the max
                 player.sendMessage(ChatColor.YELLOW + "You overrode the max station check as admin.");
                 Logger.quietLog("Plugin admin " + player.getUniqueId().toString() + " overrode station no. limit.");
             } else {
+                // Cancels
                 e.setCancelled(true);
                 player.sendMessage(ChatColor.RED + "You already have the maximum no. of stations.");
                 return;
             }
         }
 
+        // If placing is valid, it begins initialising
         Block block = e.getBlock();
-        PersistentDataController.giveCustomTag(block);
+        PersistentDataController.giveCustomTag(block); // Gives banner block tag
         BlockState state = block.getState();
         Nameable nameable = (Nameable) state;
         nameable.setCustomName("Banner");
-        state.update();
+        state.update(); // Updates block
         
-        Station newStation = new Station(
+        Station newStation = new Station( // Create station object
             UUID.randomUUID(),
             "Station " + (Station.getStations().size()+1) ,
             player,
             block.getLocation(),
             player.getLocation()
         );
-        
+        // Notify and log
         player.sendMessage(ChatColor.GREEN + "You created " + ChatColor.YELLOW + newStation.getDisplayName() + ChatColor.GREEN + ".");
         Logger.quietLog(player.getUniqueId().toString() +  " created station - " + newStation.getDisplayName() + " : " + newStation.getIdString());
         
