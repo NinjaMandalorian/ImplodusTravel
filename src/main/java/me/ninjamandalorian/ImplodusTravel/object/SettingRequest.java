3568ee7520f61f6250c5fd1336101a2c40a71757
Isaac <ninjamandalov@gmail.com>
2023-10-18 02:56:24 +0100

    Object Documentation

--- src/main/java/me/ninjamandalorian/ImplodusTravel/object/SettingRequest.java
+++ src/main/java/me/ninjamandalorian/ImplodusTravel/object/SettingRequest.java
@@ -2,13 +2,21 @@ package me.ninjamandalorian.ImplodusTravel.object;
 
 import org.bukkit.entity.Player;
 
+/** Setting Request Object
+ * @author NinjaMandalorian
+ */
 public class SettingRequest {
     
-    private Player player;
-    private ChatSettable settable;
-    private String setting;
-    private Long requestTime;
+    private Player player; // Player making request
+    private ChatSettable settable; // Settable
+    private String setting; // Key
+    private Long requestTime; // Time of request
 
+    /** Constructor
+     * @param player - Player
+     * @param settable - Settable object
+     * @param settingName - Key for setting
+     */
     public SettingRequest(Player player, ChatSettable settable, String settingName) {
         this.player = player;
         this.settable = settable;
@@ -16,18 +24,30 @@ public class SettingRequest {
         this.requestTime = System.currentTimeMillis();
     }
 
+    /** Get player
+     * @return Player
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /** Get settable
+     * @return Settable
+     */
     public ChatSettable getSettable() {
         return settable;
     }
 
+    /** Get setting
+     * @return Setting key
+     */
     public String getSetting() {
         return setting;
     }
     
+    /** Get request time
+     * @return Request time
+     */
     public Long getRequestTime() {
         return requestTime;
     }
